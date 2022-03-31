fee4297b91ec3430dbfcf057fbd4754e973a9186
Thiago Calazans <thiagotorrescalazans@gmail.com>
2022-03-31 18:41:22 -0300

    created api

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,6 +1,6 @@
-// Visit The Stimulus Handbook for more details 
+// Visit The Stimulus Handbook for more details
 // https://stimulusjs.org/handbook/introduction
-// 
+//
 // This example controller works with specially annotated HTML like:
 //
 // <div data-controller="hello">
@@ -9,10 +9,26 @@
 
 import { Controller } from "stimulus"
 
+
+
+const publicKey = "354b64a846079737d78c7b7ab3a299ea"
+
+
+const hash = "a904ee4ee86230876bf125b5355e9024"
+
+const baseURL = `https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${publicKey}&hash=${hash}`;
+
+
+
 export default class extends Controller {
-  static targets = [ "output" ]
+  // static targets = [ "output" ]
 
   connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+
+    fetch(baseURL)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+      })
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show] do
+    resources :lists, except:[:delete, :patch] do
+      resources :bookmarks, only: [ :new, :create ]
+    end
+  end
+  resources :hero_display, only:[:index, :show]
+  resources :bookmarks, only: [ :destroy, :edit, :update ]
 end
